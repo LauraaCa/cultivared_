f1c7a812243baa3e26cb9ee3d627846f3fa17a07
AndresP05 <apuentesm2@ucentral.edu.co>
2025-04-01 13:50:24 -0500

    Mejoras comprador (Home, Perfil)
    Se mejora el apartado de filtros del home, como se presentan las tarjertas.
    Se adiciona el apartado de perfil.

--- src/routes/comprador.py
+++ src/routes/comprador.py
@@ -26,4 +26,22 @@ def comprador():
     
     return """<script> alert("Por favor, primero inicie sesión."); window.location.href = "/CULTIVARED/login"; </script>"""
 
+@main.route('/perfil')
+def perfil():
+    if 'logueado' in session and session['logueado']:
+        conn = get_connection()
+        cur = conn.cursor()
+        
+        # Obtener datos del usuario autenticado
+        cur.execute('SELECT * FROM usuarios WHERE email = %s', (session['email'],))
+        user = cur.fetchone()
+        
+        cur.close()
+        conn.close()
 
+        if user:
+            return render_template('comprador/perfilComprador.html', user=user)
+        else:
+            return """<script> alert("Usuario no encontrado."); window.location.href = "/CULTIVARED/login"; </script>"""
+    
+    return """<script> alert("Por favor, primero inicie sesión."); window.location.href = "/CULTIVARED/login"; </script>"""
