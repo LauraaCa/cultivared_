ace7d66606bc9307fbc68d3ab2b3402b52c09a5a
AndrewP05 <apuentesm2@ucentral.edu.co>
2025-05-05 00:38:28 -0500

    prueba

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\nicol\cultivared_\venv"
+set "VIRTUAL_ENV=C:\Users\tibur\Desktop\cultivared_\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
