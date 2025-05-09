cf67eb3548e34f4a881f02cccb832b5951e9f58e
egalanm1232 <egalanm@ucentral.edu.co>
2025-05-09 18:31:37 -0500

    Agregadas estadísticas de usuarios y productos

--- src/routes/comprador.py
+++ src/routes/comprador.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask import Blueprint, Response, abort, render_template, request, redirect, url_for, session, flash
 from config import get_connection  # Conexión a PostgreSQL
 
 main = Blueprint('comprador_blueprint', __name__)
